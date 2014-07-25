140dad047f55c66ecddd69d46aadaee96e0bcd7a
Christian Sloth Krogh <you@example.com>
2014-07-25 19:59:21 +0200

    * final changes to the floraGeneration script

---
+++ Assets/Testing/Day&NightCycle/script_day&nightCycle.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+///////////////////////////////////////////////////////////////////////////
+////////////////////////// day&night cycle script /////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+
+		var sunObject		: Transform;
+private	var dayLengthInSecs : int			= 300;
+
+
+function Start () 
+{
+
+	
+
+}
+
+function Update () 
+{	
+	SunController ();
+}
+
+
+function SunController ()
+{
+	// Spin the object around the world origin at 20 degrees/second.
+	
+	sunObject.transform.RotateAround (Vector3.zero, Vector3.right, 360 * Time.deltaTime / dayLengthInSecs);
+}
